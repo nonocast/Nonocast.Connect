af70b94e55697d1d7eade51e56c816ad86664913
canmusic <canmusic@gmail.com>
2014-10-11 17:49:06 +0800

    解决WebSocket断线重连bug

--- Nonocast.Connect/WebSocketImpl/WebSocket.cs
+++ Nonocast.Connect/WebSocketImpl/WebSocket.cs
@@ -10,6 +10,8 @@ using System.Timers;
 namespace Nonocast.Connect.WebSocket {
 	public class WebSocket {
 		public event Action<string> MessageReceived;
+		public event Action<bool> ConnectionChanged;
+		public event Action Connecting;
 
 		public WebSocket(string url) {
 			this.url = url;
@@ -25,20 +27,17 @@ namespace Nonocast.Connect.WebSocket {
 		}
 
 		public void Open() {
-			if(_opening) return;
+			StopReconnect();
 			try {
-				_opening = true;
 				client = new TcpClient();
+				if(Connecting != null) Connecting();
 				client.Connect(hostname, port);
 				this.stream = client.GetStream();
 				Handshake();
 				new Thread(new ParameterizedThreadStart(Process)).Start(this.stream);
 				Connected = true;
-				_opening = false;
-			} catch {// offline and try reconnect
+			} catch { // offline and try reconnect
 				Connected = false;
-				_opening = false;
-				return;
 			}
 		}
 
@@ -57,7 +56,7 @@ namespace Nonocast.Connect.WebSocket {
 			// Console.WriteLine("Thread Enter...");
 			var stream = arg as NetworkStream;
 
-			if(stream == null || stream.CanRead == false || stream.CanWrite == false || stream.DataAvailable == false) {
+			if(stream == null) {
 				Connected = false;
 				return;
 			}
@@ -87,41 +86,37 @@ namespace Nonocast.Connect.WebSocket {
 			var buffer = new ClientFrame(new TextMessage(message)).ToBytes();
 			try {
 				stream.Write(buffer, 0, buffer.Length);
-			} catch(IOException) {// offline and try reconnect
+			} catch(IOException) { // offline and try reconnect
 				Connected = false;
 			}
 		}
 
 		public void Close() {
+			StopReconnect();
 			try { if(stream != null) stream.Close(); } catch { }
 			try { if(client != null) client.Close(); } catch { }
 			stream = null;
 			client = null;
-			_connected = false;
+			connected = false;
 		}
 
 		private void StartReconnect() {
-			if(connTimer != null && connTimer.Enabled) return;
-
+			if(reconnectTimer != null) return;
 			Close();
-			connTimer = new System.Timers.Timer(2000);
-			connTimer.Elapsed += connTimer_Elapsed;
-			connTimer.Start();
+			reconnectTimer = new System.Threading.Timer(new TimerCallback((o) => Open()), this, 0, 2000);
 		}
 
 		private void StopReconnect() {
-			try { connTimer.Stop(); } catch { }
-			try { connTimer.Close(); } catch { }
-		}
-
-		private void connTimer_Elapsed(object sender, ElapsedEventArgs e) {
-			Open();
+			if(reconnectTimer == null) return;
+			try { reconnectTimer.Dispose(); } catch { }
+			reconnectTimer = null;
 		}
 
 		public bool Connected {
-			get { return _connected; }
+			get { return connected; }
 			set {
-				_connected = value;
+				if(connected != value && ConnectionChanged != null) ConnectionChanged(value);
+				connected = value;
 				if(value) {
 					StopReconnect();
 				} else {
@@ -136,8 +131,7 @@ namespace Nonocast.Connect.WebSocket {
 		private string hostname;
 		private int port;
 		private string url;
-		private bool _connected = false;
-		private bool _opening = false;
-		private System.Timers.Timer connTimer;
+		private bool connected = false;
+		private System.Threading.Timer reconnectTimer;
 	}
 }
--- Shell/WebSocketShell/Program.cs
+++ Shell/WebSocketShell/Program.cs
@@ -8,15 +8,24 @@ using System.Threading;
 namespace Nonocast.Connect.WebSocket.Shell {
 	class Program {
 		static void Main(string[] args) {
-			var ws = new WebSocket("ws://localhost:8000/x");
+			//var ws = new WebSocket("ws://localhost:8000/x");
+			var ws = new WebSocket("ws://192.168.10.251:8080/x");
 			ws.MessageReceived += (message) => Console.WriteLine(message);
+			ws.ConnectionChanged += (isConnected) => {
+				if(isConnected)
+					Console.WriteLine("Connected");
+				else
+					Console.WriteLine("Disconnected");
+			};
+			ws.Connecting += () => Console.WriteLine("Connecting...");
 			ws.Open();
 
 			Console.WriteLine("press any key to exit.");
 			while(true) {
 				try {
-					Thread.Sleep(100);
-					ws.Emit("world hello");
+					string line = Console.ReadLine();
+					if(line == "exit") break;
+					ws.Emit(line);
 				} catch(SocketException) { }
 			}
 			ws.Close();
